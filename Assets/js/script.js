936f38726f5a5d3bbef88dd67e150259e7fec15c
k8 <katesterchi@gmail.com>
2023-07-10 07:08:55 -0500

    began psuedo coding in JS what variable assignments I will need

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+// 1 define and assign values to global variables
+// 2 create functions
+// 3 call functions
+
+// create an array of obejects, each object holds the data for each question
+// remember to separate by commas
+
+// assign the quiz id to a variable
+// assign the answer id to a variable
+// assign the question id to a variable
+// assign each x_text id to a variable
+// assign the submit button to a variable
+
