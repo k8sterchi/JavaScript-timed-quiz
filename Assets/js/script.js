c5a3dd3e01a312b0d389038f57b598dfa7ba5f3d
k8 <katesterchi@gmail.com>
2023-07-13 03:00:33 -0500

    added a class to p tag timer in HTML, continued with display and click event functions in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,7 @@
 //write functions to handle the button clicks and change what is displayed
 
 var startButton = document.querySelector(".quiz-intro button");
+var timerDisplay = document.querySelector(".top-bar p");
 var questionScreen = document.querySelector(".question-screen");
 var finishScreen = document.querySelector(".quiz-finish-screen");
 var highScoresScreen = document.querySelector(".highscores-screen");
@@ -25,40 +26,81 @@ var quizData = [
       answers: ["call.newFunction()", "newFunction().call", "initiate:newFunction()", "newFunction()"],
       correctAnswer: "newFunction()"
     }]
-    
-// Add event listener to the start button
-startButton.addEventListener("click", startQuiz);
-console.log ('start button clicked')
 
-// Function to handle start quiz button click event
+// Timer and quiz state variables 
+var currentQuestionIndex = 0;
+var score = 0;
+var timeRemaining = 60;
+var timerInterval;
+
+// Add eventlistener to the start button
+startButton.addEventListener("click", startQuiz);
+
+// Function to handle start quiz button click event and to start the quiz
 function startQuiz() {
-  // Hide intro section and show question screen
+  console.log('The start button has been click, quiz begins')  
   document.querySelector(".quiz-intro").style.display = "none";
   questionScreen.style.display = "block";
+  displayQuestion();
+  startTimer(); // Start the timer
 }
 
-// Function to start the quiz
-function startQuiz() {
-    document.querySelector(".quiz-intro").style.display = "none";
-    questionScreen.style.display = "block";
-    displayQuestion();
-    // Other code to handle the quiz logic
+// Function to start the timer
+function startTimer() {
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+// Function to update the timer
+function updateTimer() {
+  if (timeRemaining > 0) {
+    timeRemaining--;
+    var minutes = Math.floor(timeRemaining / 60).toString().padStart(2, "0");
+    var seconds = (timeRemaining % 60).toString().padStart(2, "0");
+    timerDisplay.textContent = `Timer: ${minutes}:${seconds}`;
+  } else {
+    finishQuiz();
   }
+}
+
+// Function to display a question and its answers
+function displayQuestion() {
+    var currentQuestion = quizData[currentQuestionIndex];
+    var questionElement = document.querySelector(".question-screen h2");
+    var answerElements = document.querySelectorAll(".question-screen li button");
   
-  // Function to display a question
-  function displayQuestion() {
-    const questionElement = document.querySelector(".question-screen h2");
-    const answerElements = document.querySelectorAll(".question-screen li button");
-  
-    // Retrieve the first question from the quizData array
-    const currentQuestion = quizData[0];
-  
-    // Display the question text
     questionElement.textContent = currentQuestion.question;
   
-    // Display the answer options
     for (let i = 0; i < answerElements.length; i++) {
       answerElements[i].textContent = currentQuestion.answers[i];
+      answerElements[i].addEventListener("click", () => {
+        checkAnswer(answerElements[i].textContent);
+      });
     }
   }
+  
+  // Function to check the selected answer
+  function checkAnswer(selectedAnswer) {
+    var currentQuestion = quizData[currentQuestionIndex];
+  
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      score++;
+      currentQuestionIndex++;
+      if (currentQuestionIndex < quizData.length) {
+        displayQuestion();
+      } else {
+        finishQuiz();
+      }
+    } else {
+      timeRemaining -= 10; // Subtract 10 seconds 
+      if (timeRemaining <= 0) {
+        finishQuiz();
+      }
+    }
+  }
+  
+
+// Event listener for the Start button
+startButton.addEventListener("click", startQuiz);
+
+
   
